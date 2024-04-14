818bd781e5fd9f8a1d5baad1ffc42507ed6339b8
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2024-04-14 22:13:54 +0530

    Created Proper Functionality

--- index.js
+++ index.js
@@ -1,57 +1,44 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
 
-const app = express();
-app.arguments(bodyParser.json());
-app.arguments(express.static("public"));
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+const app= express()
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
-mongoose.connect("mongodb://localhost:27017/MoneyList");
-var db = mongoose.connection;
-db.on("error", () => {
-  console.log("Error in connecting DB");
-});
+mongoose.connect('mongodb://localhost:27017/MoneyList')
+var db = mongoose.connection
+db.on('error', ()=> console.log("Error in connecting to the Database"))
+db.once('open', () => console.log("Connected to Database"))
 
-db.once("open", () => {
-  console.log("Connected Successfully!");
-});
+app.post("/add", (req,res) =>{
+    var category_select = req.body.category_select
+    var amount_input= req.body.amount_input
+    var info = req.body.info
+    var date_input = req.body.date_input
 
-app.post("/add", (req, res) => {
-  var category_select = req.body.category_select;
-  var amount_input = req.body.amount_input;
-  var info = req.body.info;
-  var date_input = req.body.date_input;
+    var data={
+        "Category": category_select,
+        "Amount" : amount_input,
+        "Info": info,
+        "Date": date_input
+    }
+    db.collection('users').insertOne(data, (err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully")
 
-  var data = {
-    Category: category_select,
-    // 'Amount': parseInt(amount_input),
-    Amount: amount_input,
-    Info: info,
-    // 'Date': new Date(date_input)
-    Date: date_input,
-  };
-  db.collection(
-    "moneyData".insertOne(data, (err, collection) => {
-      if (!err) {
-        throw err;
-      }
-      console.log("Request Inserted Successfully!");
     })
-  );
-});
-
-app
-  .get("/", (req, res) => {
+})
+app.get("/",(req,res) =>{
     res.set({
-      "Allow-access-Allow-Origin": "*",
-    });
-    return res.redirect("index.html");
-  })
-  .listen(5000);
+        "Allow-access-Allow-Origin":'*'
+    })
+    return res.redirect('index.html')
+}).listen(5000)
 
-console.log("Listening on port 5000");
+console.log("Listening on port 5000")
--- public/script.js
+++ public/script.js
@@ -1,112 +1,112 @@
-let expenses = [];
+let expenses =[]
 let totalAmount = 0;
-const categorySelect = document.getElementById("category_select");
-const amountInput = document.getElementById("amount_input");
-const infoInput = document.getElementById("info");
-const dateInput = document.getElementById("date_input");
-const addBtn = document.getElementById("add_btn");
-const expensesTableBody = document.getElementById("expenses-table-body");
-const totalAmountCell = document.getElementById("total-amount");
+const categorySelect = document.getElementById('category_select')
+const amountInput = document.getElementById('amount_input')
+const InfoInput = document.getElementById('info')
+const dateInput = document.getElementById('date_input')
+const addBtn = document.getElementById('add_btn')
+const expenseTableBody = document.getElementById('expense-table-body')
+const totalAmountCell = document.getElementById('total-amount')
 
-addBtn.addEventListener("click", function () {
-  const category = categorySelect.value;
-  const info = infoInput.value;
-  const amount = Number(amountInput.value);
-  const date = dateInput.value;
+addBtn.addEventListener('click',function(){
+    const category=categorySelect.value;
+    const info=InfoInput.value;
+    const amount =Number(amountInput.value);
+    const date = dateInput.value;
 
-  if (category === "") {
-    alert("Please select a Category!");
-    return;
-  }
-  if (isNaN(amount) || amount <= 0) {
-    alert("Please enter a valid amount!");
-    return;
-  }
-  if (info === "") {
-    alert("Please enter a valid info!");
-    return;
-  }
-  if (category === "") {
-    alert("Please select a date!");
-    return;
-  }
-
-  expenses.push({ category, amount, info, date });
-  if (category === "Income") {
-    totalAmount += amount;
-  }
-  if (category === "Expense") {
-    totalAmount -= amount;
-  }
-  totalAmountCell.textContent = totalAmount;
-
-  const newRow = expensesTableBody.insertRow();
-
-  const categoryCell = newRow.insertCell();
-  const amountCell = newRow.insertCell();
-  const infoCell = newRow.insertCell();
-  const dateCell = newRow.insertCell();
-  const deleteCell = newRow.insertCell();
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "Delete";
-  deleteBtn.classList.add("delete-btn");
-  deleteBtn.addEventListener("click", function () {
-    expenses.splice(expenses.indexof(expenses), 1);
-    if (category === "Income") {
-      totalAmount -= amount;
+    if(category ===''){
+        alert('please select a category');
+        return;
     }
-    if (category === "Expense") {
-      totalAmount += amount;
+    if(isNaN(amount) || amount<=0){
+        alert('please enter a valid amount');
+        return;
     }
+    if(info ===''){
+        alert('please enter a valid amount info');
+        return;
+    }
+    if(date ===''){
+        alert('please select a date');
+        return;
+    }
+    expenses.push({category,amount,info,date})
+    if(category === 'Income'){
+        totalAmount+=amount;
+    }
+    if(category === 'Expense'){
+        totalAmount-=amount;
+    }
+     totalAmountCell.textContent = totalAmount;
 
-    totalAmountCell.textContent = totalAmount;
-    expensesTableBody.removeChild(newRow);
-  });
-  const expense = expenses[expenses.length - 1];
-  categoryCell.textContent = expense.category;
-  amountCell.textContent = expense.amount;
-  infoCell.textContent = expense.info;
-  dateCell.textContent = expense.date;
-  deleteCell.appendChild(deleteBtn);
+     const newRow = expenseTableBody.insertRow();
+
+     const categoryCell = newRow.insertCell();
+     const AmountCell = newRow.insertCell();
+     const InfoCell = newRow.insertCell();
+     const dateCell = newRow.insertCell();
+     const deleteCell = newRow.insertCell();
+
+     const deleteBtn = document.createElement('button');
+     deleteBtn.textContent ='Delete';
+     deleteBtn.classList.add('delete-btn');
+     deleteBtn.addEventListener('click', function(){
+        expenses.splice(expenses.indexOf(expense),1);
+        if(category === 'Income'){
+            totalAmount-=amount;
+        }
+        if(category === 'Expense'){
+            totalAmount+=amount;
+        }
+
+        totalAmountCell.textContent=totalAmount;
+        expenseTableBody.removeChild(newRow)
+     })
+     const expense = expenses[expenses.length-1];
+     categoryCell.textContent=expense.category;
+     AmountCell.textContent=expense.amount;
+     InfoCell.textContent=expense.info;
+     dateCell.textContent=expense.date;
+     deleteCell.appendChild(deleteBtn);
+
+
 });
-
-for (const expense of expenses) {
-  if (category === "Income") {
-    totalAmount += amount;
-  }
-  if (category === "Expense") {
-    totalAmount -= amount;
-  }
-  totalAmountCell.textContent = totalAmount;
-
-  const newRow = expensesTableBody.insertRow();
-
-  const categoryCell = newRow.insertCell();
-  const amountCell = newRow.insertCell();
-  const infoCell = newRow.insertCell();
-  const dateCell = newRow.insertCell();
-  const deleteCell = newRow.insertCell();
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "Delete";
-  deleteBtn.classList.add("delete-btn");
-  deleteBtn.addEventListener("click", function () {
-    expenses.splice(expenses.indexof(expenses), 1);
-    if (category === "Income") {
-      totalAmount -= amount;
+for(const expense of expenses){
+    if(category === 'Income'){
+        totalAmount+=amount;
     }
-    if (category === "Expense") {
-      totalAmount += amount;
+    if(category === 'Expense'){
+        totalAmount-=amount;
     }
+     totalAmountCell.textContent = totalAmount;
 
-    totalAmountCell.textContent = totalAmount;
-    expensesTableBody.removeChild(newRow);
-  });
-  const expense = expenses[expenses.length - 1];
-  categoryCell.textContent = expense.category;
-  amountCell.textContent = expense.amount;
-  infoCell.textContent = expense.info;
-  dateCell.textContent = expense.date;
-  deleteCell.appendChild(deleteBtn);
+     const newRow = expenseTableBody.insertRow();
+
+     const categoryCell = newRow.insertCell();
+     const AmountCell = newRow.insertCell();
+     const InfoCell = newRow.insertCell();
+     const dateCell = newRow.insertCell();
+     const deleteCell = newRow.insertCell();
+
+     const deleteBtn = document.createElement('button');
+     deleteBtn.textContent ='Delete';
+     deleteBtn.classList.add('delete-btn');
+     deleteBtn.addEventListener('click', function(){
+        expenses.splice(expenses.indexOf(expense),1);
+        if(category === 'Income'){
+            totalAmount-=amount;
+        } 
+        if(category === 'Expense'){
+            totalAmount+=amount;
+        }
+
+        totalAmountCell.textContent=totalAmount;
+        expenseTableBody.removeChild(new Row)
+     })
+     const expense = expenses[expenses.length-1];
+     categoryCell.textContent=expense.category;
+     AmountCell.textContent=expense.amount;
+     InfoCell.textContent=expense.info;
+     dateCell.textContent=expense.date;
+     deleteCell.appendChild(deleteBtn);
 }
